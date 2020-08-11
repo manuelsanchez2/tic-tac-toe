05b26719d3e8c401d8859ab8f5ff2b5a448f0f08
manuelsanchez2 <manusansan22@gmail.com>
2020-08-11 11:31:58 +0200

    Create an alert function by clicking

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,5 +2,14 @@ import "./square.css";
 import React from "react";
 
 export default function Square(props) {
-  return <button className="square">{props.value}</button>;
+  return (
+    <button 
+    className="square" 
+    onClick={() => {
+    alert("click");
+    }}
+    >
+      {props.value}
+    </button>
+  );
 }
