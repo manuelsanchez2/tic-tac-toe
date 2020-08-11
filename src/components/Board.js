2bcb844ee7f4ce10b920dae39d41201de0a6d8ae
manuelsanchez2 <manusansan22@gmail.com>
2020-08-11 14:46:37 +0200

    Apply the individual logic to each square

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,21 +3,32 @@ import React from "react";
 import Square from "./Square";
 
 export default function Board() {
-  const status = "Next player: X";
 
+  const [squares, setSquares] = React.useState([
+    "🐼",null,"🐶",
+    "🐶","🐶","🐼",
+    "🐼",null,null,
+  ]);
+
+  const status = "Next player: 🐶";
+
+  const handleClick = (index) => {
+    alert("INDEX: " + index);
+    setSquares(["🐼",null,"🐶","🐶","🐶","🐶","🐶",null,null,])
+  }
   return (
     <div>
       <div className="status">{status}</div>
-      <div className="board">
-        <Square value="0" />
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
-        <Square value="7" />
-        <Square value="8" />
+      <div className="board" >
+        <Square value={squares[0]} onClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onClick={() => handleClick(2)}/>
+        <Square value={squares[3]} onClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onClick={() => handleClick(5)}/>
+        <Square value={squares[6]} onClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onClick={() => handleClick(8)}/>
       </div>
     </div>
   );
--- src/components/Square.js
+++ src/components/Square.js
@@ -2,11 +2,12 @@ import "./square.css";
 import React from "react";
 
 export default function Square(props) {
+
   return (
     <button 
     className="square" 
     onClick={() => {
-    alert("click");
+      props.onClick();
     }}
     >
       {props.value}
