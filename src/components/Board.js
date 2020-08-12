8625b499b901674b3aae728e2b7b1f72870a372c
Manuel Sanchez <68222396+manuelsanchez2@users.noreply.github.com>
2020-08-12 09:25:31 +0200

    Solution (#1)
    * Make numbers come up in the square
    
    * Create an alert function by clicking
    
    * Apply the individual logic to each square
    
    * Add slice method for the squares
    
    * Lifting State Up
    
    * Add pandaIsNext logic
    
    * Declare the winner in the game
    
    * Make the board bigger
    
    * Center the title and make the font bigger

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,52 @@
 import "./board.css";
 import React from "react";
 import Square from "./Square";
+import { calculateWinner } from "../api/game";
+
 
 export default function Board() {
-  const status = "Next player: X";
 
+  // const [squares, setSquares] = React.useState([
+  //   null,null,null,
+  //   null,null,null,
+  //   null,null,null,
+  // ]);
+  const [squares, setSquares] = React.useState(Array(9).fill(null))
+  const [pandaIsNext, setPandaIsNext] = React.useState(true);
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "The winner is " + winner;
+  } else {
+    status = pandaIsNext ? "Next player: 🐼" : "Next player: 🐶";
+  }
+
+  const handleClick = (index) => {
+    //copy squares -- shallow copy / flache Kopie - si no le decimos start, end, va a empezar desde el principio...
+    
+    const squaresCopy = squares.slice();
+
+    //modify value by index
+    squaresCopy[index] = pandaIsNext ? "🐼" : "🐶";
+    // squaresCopy[index] = "🐼";
+    //set me the state
+    setSquares(squaresCopy);
+    setPandaIsNext(!pandaIsNext);
+  }
   return (
     <div>
       <div className="status">{status}</div>
-      <div className="board">
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
+      <div className="board" >
+        <Square value={squares[0]} onClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onClick={() => handleClick(2)}/>
+        <Square value={squares[3]} onClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onClick={() => handleClick(5)}/>
+        <Square value={squares[6]} onClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onClick={() => handleClick(8)}/>
       </div>
     </div>
   );
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,14 @@
 import "./square.css";
 import React from "react";
 
-export default function Square() {
-  return <button className="square">{/* TODO */}</button>;
+export default function Square(props) {
+
+  return (
+    <button 
+    className="square" 
+    onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
